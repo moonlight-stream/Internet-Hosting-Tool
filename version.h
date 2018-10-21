c35241f7d60cfddf34b8966f018b214b1b1ccf4e
Cameron Gutman <aicommander@gmail.com>
2018-10-20 20:01:59 -0700

    Version 1.1

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define VER_COMPANYNAME_STR         "Moonlight Game Streaming Project"
-#define VER_FILEVERSION             1,0,0,0
-#define VER_FILEVERSION_STR         "1.0.0.0\0"
+#define VER_FILEVERSION             1,1,0,0
+#define VER_FILEVERSION_STR         "1.1.0.0\0"
