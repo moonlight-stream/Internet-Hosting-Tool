44625583b930d2eb3a5272cc33b01d7faaf4435b
Cameron Gutman <aicommander@gmail.com>
2018-10-20 18:47:36 -0700

    Unify the version information

--- version.h
+++ version.h
@@ -3,4 +3,3 @@
 #define VER_COMPANYNAME_STR         "Moonlight Game Streaming Project"
 #define VER_FILEVERSION             1,0,0,0
 #define VER_FILEVERSION_STR         "1.0.0.0\0"
-#define VER_FILEDESCRIPTION_STR     "Moonlight Internet Streaming Service"
