2d31cd0420c14337fd4d4e48e631c933b9d1add7
Cameron Gutman <aicommander@gmail.com>
2019-07-28 12:33:05 -0700

    Rename Internet Streaming Helper to Internet Hosting Tool

--- version.h
+++ version.h
@@ -4,4 +4,4 @@
 #define VER_VERSION_STR             "4.0.0.0"
 
 #define VER_COMPANYNAME_STR         "Moonlight Game Streaming Project"
-#define VER_PRODUCTNAME_STR         "Moonlight Internet Streaming Helper"
+#define VER_PRODUCTNAME_STR         "Moonlight Internet Hosting Tool"
