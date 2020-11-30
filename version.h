fa5ae73abb198d338d37e05763e12d943dd1f887
Cameron Gutman <aicommander@gmail.com>
2020-11-29 23:38:14 -0600

    Version 5.5.1

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VER_VERSION                 5,5,0,0
-#define VER_VERSION_STR             "5.5.0.0"
+#define VER_VERSION                 5,5,1,0
+#define VER_VERSION_STR             "5.5.1.0"
 
 #define VER_COMPANYNAME_STR         "Moonlight Game Streaming Project"
 #define VER_PRODUCTNAME_STR         "Moonlight Internet Hosting Tool"
